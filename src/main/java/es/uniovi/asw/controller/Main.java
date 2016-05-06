bd0accd59d31edf1187ddeebc3a710288da7f011
Mónica Cueva González <mcglezoviedo@outlook.es>
2016-05-06 14:16:14 +0200

    cambios

--- src/main/java/es/uniovi/asw/controller/Main.java
+++ src/main/java/es/uniovi/asw/controller/Main.java
@@ -204,7 +204,7 @@ public class Main {
 
 	}
 
-	@RequestMapping(value = "/modificar_elecciones")
+	@RequestMapping(value = "/referendum", method = RequestMethod.POST)
 	public String modificar(Elecciones elecciones, Model model) {
 		LOG.info("Modificar elecciones page access");
 		return "/modificar_elecciones";
